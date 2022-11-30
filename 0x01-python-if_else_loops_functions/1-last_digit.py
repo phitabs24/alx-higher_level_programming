2d87d153911dee7b726a6b90254ce89c37334361
phitabs24 <107364663+phitabs24@users.noreply.github.com>
2022-11-30 03:31:55 +0100

    prints a signed last digit of a random number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,8 @@ if number < 0:
 else:
     last = number % 10
 if last > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last))
+    print(F"Last digit of {number} is {last} and is greater than 5")
 elif last == 0:
-    print("Last digit of {} is {} and is 0".format(number, last))
+    print(F"Last digit of {number} is {last} and is 0")
 elif last < 6 and last != 0:
-    print("Last digit of {} is {} and is less\
- than 6 and not 0".format(number, last))
+    print(F"Last digit of {number} is {last} and is less than 6 and not 0")
